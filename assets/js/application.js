19b96bd7063fcf58ef3acf58d74db1ca72dcc671
Tymur Daudov <tymfear@gmail.com>
2016-11-23 16:59:50 +0200

    ui

---
+++ assets/js/application.js
@@ -0,0 +1,5 @@
+'use strict';
+
+(function(){
+  angular.module('mutokukai',['ui.router'])
+})();
---
+++ assets/js/routes.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function(){
+  angular.module('mutokukai')
+    .config([ '$stateProvider','$urlRouterProvider',
+      function($stateProvider, $urlRouterProvider){
+        $stateProvider
+          .state('home', {
+            url: '/',
+            templateUrl: 'assets/js/home/home.html'
+          })
+          .state('aikido', {
+            url: '/aikido',
+            templateUrl: 'assets/js/aikido/aikido.html'
+          })
+          .state('mutokukai', {
+            url: '/mutokukai',
+            templateUrl: 'assets/js/mutokukai/mutokukai.html'
+          })
+          .state('about', {
+            url: '/about',
+            templateUrl: 'assets/js/about/about.html'
+          })
+          .state('contacts', {
+            url: '/contacts',
+            templateUrl: 'assets/js/contacts/contacts.html'
+          });
+
+        $urlRouterProvider.otherwise('/')
+      }]);
+})();
