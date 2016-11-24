ef17119b8f8c1f56294a1119cc28df46d2eb028a
Tymur Daudov <tymfear@gmail.com>
2016-11-24 11:51:59 +0200

    t

--- assets/js/routes.config.js
+++ assets/js/routes.config.js
@@ -7,23 +7,23 @@
         $stateProvider
           .state('home', {
             url: '/',
-            templateUrl: 'assets/js/home/home.html'
+            templateUrl: '/assets/js/home/home.html'
           })
           .state('aikido', {
             url: '/aikido',
-            templateUrl: 'assets/js/aikido/aikido.html'
+            templateUrl: '/assets/js/aikido/aikido.html'
           })
           .state('mutokukai', {
             url: '/mutokukai',
-            templateUrl: 'assets/js/mutokukai/mutokukai.html'
+            templateUrl: '/assets/js/mutokukai/mutokukai.html'
           })
           .state('about', {
             url: '/about',
-            templateUrl: 'assets/js/about/about.html'
+            templateUrl: '/assets/js/about/about.html'
           })
           .state('contacts', {
             url: '/contacts',
-            templateUrl: 'assets/js/contacts/contacts.html'
+            templateUrl: '/assets/js/contacts/contacts.html'
           });
 
         $locationProvider.html5Mode({enabled:true, requireBase: false});
