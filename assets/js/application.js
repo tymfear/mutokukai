157fbb03f8e9ea896ae9b1f20a34dcd69f07c7d6
Tymur Daudov <tymfear@gmail.com>
2016-11-30 22:47:51 +0200

    remove translate-cloak

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,15 +1,14 @@
 'use strict';
 
-(function(){
+(function () {
   angular.module('mutokukai', ['ui.router', 'pascalprecht.translate'])
-    .config(['$translateProvider', '$translatePartialLoaderProvider', '$compileProvider',
-      function($translateProvider, $translatePartialLoaderProvider, $compileProvider){
-      $translateProvider.useLoader('$translatePartialLoader', {
-        urlTemplate: './assets/translations/{lang}/{part}.json'
-      });
+    .config(['$translateProvider', '$compileProvider',
+      function ($translateProvider, $compileProvider) {
+        $translateProvider.useLoader('$translatePartialLoader', {
+          urlTemplate: './assets/translations/{lang}/{part}.json'
+        });
 
-      $translateProvider.preferredLanguage('ru-RU');
-
-      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|skype):/);
-    }]);
+        $translateProvider.preferredLanguage('ru-RU');
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|skype):/);
+      }]);
 })();
