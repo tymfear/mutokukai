71138e22d8a05afe99f5e176e2b3a07d357831cd
seliverstmax <154024685+seliverstmax@users.noreply.github.com>
2023-12-17 00:41:23 +0200

    Update instructor.service.js
    переклад

--- app/js/common/instructor.service.js
+++ app/js/common/instructor.service.js
@@ -9,7 +9,7 @@
         instructors.list = [{
                 id: 1,
                 instructorName: 'Даудов Тимур',
-                instructorRank: '3-й дан Айкидо Айкикай',
+                instructorRank: '3-й дан Айкідо Айкікай',
                 skype: 'tymfear',
                 email: 'tymfear@gmail.com',
                 phones: ['38-066-212-90-26']
@@ -17,7 +17,7 @@
             {
                 id: 2,
                 instructorName: 'Колтун Андрей',
-                instructorRank: '3-й дан Айкидо Айкикай',
+                instructorRank: '3-й дан Айкідо Айкікай',
                 email: 'samuray_studio@mail.ru',
                 phones: ['38-066-630-24-24', '38-097-946-41-39'],
                 skype: 'deltaandrey'
@@ -25,14 +25,14 @@
             {
                 id: 3,
                 instructorName: 'Луговой Николай',
-                instructorRank: '2-й дан Айкидо Айкикай',
+                instructorRank: '2-й дан Айкідо Айкікай ',
                 email: 'varan_com@ukr.net',
                 phones: ['38-095-259-77-08']
             },
             {
                 id: 4,
-                instructorName: 'Кривошей Анатолий',
-                instructorRank: '4-й дан Айкидо Айкикай',
+                instructorName: 'Кривошей Анатолій',
+                instructorRank: '4-й дан Айкідо Айкікай',
                 phones: ['38-050-592-92-09']
             }
         ];
