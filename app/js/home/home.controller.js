21d868bcdea53497d5772fc85a797d31ff6e84ab
seliverstmax <154024685+seliverstmax@users.noreply.github.com>
2023-12-17 00:36:37 +0200

    Update home.controller.js
    обновление данных на странице хоум.
    Вынес транспорт в переменные

--- app/js/home/home.controller.js
+++ app/js/home/home.controller.js
@@ -8,68 +8,15 @@
 
     function HomeController($translate, $translatePartialLoader) {
         var home = this;
-
+        var bus = "bus";
+        var train = "train";
+        
         $translatePartialLoader.addPart('home');
         $translatePartialLoader.addPart('common');
 
         home.classesSchedule = [{
-                place: 'Pushkinskaya',
-                transport: 'train',
-                schedule: {
-                    1: '6:30',
-                    3: '6:30',
-                    5: '6:30',
-                    6: '6:30'
-                }
-            },
-            {
-                place: 'Sportivnaya',
-                transport: 'train',
-                schedule: {
-                    1: '17:00',
-                    3: '17:00',
-                    5: '17:00'
-                }
-            },
-            {
-                place: 'Nauchnaya',
-                transport: 'train',
-                schedule: {
-                    1: '18:00',
-                    3: '18:00',
-                    5: '18:00'
-                }
-            },
-            {
-                place: 'Pesochin',
-                transport: 'bus',
-                schedule: {
-                    2: '19:00',
-                    4: '19:00',
-                    6: '10:00'
-                }
-            },
-            {
-                place: 'Saltovka',
-                transport: 'bus',
-                schedule: {
-                    1: '19:00',
-                    3: '19:00',
-                    5: '19:00'
-                }
-            },
-            {
-                place: 'Saltovka',
-                transport: 'bus',
-                schedule: {
-                    1: '20:30',
-                    3: '20:30',
-                    5: '20:30'
-                }
-            },
-            {
                 place: 'Maselskoho',
-                transport: 'train',
+                transport: train,
                 schedule: {
                     2: '07:00',
                     5: '07:00',
