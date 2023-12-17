019f1419bf7aac147143569d81557fbc0f3a8a81
seliverstmax <154024685+seliverstmax@users.noreply.github.com>
2023-12-17 19:39:20 +0200

    Update contacts.controller.js
    Починил список контактов

--- app/js/contacts/contacts.controller.js
+++ app/js/contacts/contacts.controller.js
@@ -21,30 +21,30 @@
                 schedule: 'Пн, Ср, Пт: 6:30 - 8:15',
                 instructorId: instructorIds.Anatoliy
             },
-            {
+        {
                 ageCategory: 'Дорослі',
                 address: 'пр-т Героїв Харкова, 246-б, ДЮСШ №7',
                 schedule: 'Пн, Пт: 07:00 - 08:30',
                 instructorId: instructorIds.Anatoliy
-            }
-{
+        },
+        {
                 ageCategory: 'Діти',
                 address: ' м. Мерефа, вул. Дніпровська, 226 ( будівля Укртелекому, 3 поверх )',
                 schedule: 'Пн, Ср, Пт: 14:15 - 15:15',
                 instructorId: instructorIds.Anatoliy
-            }
+         },
         {
                 ageCategory: 'Підлітки',
                 address: 'м. Мерефа, вул. Дніпровська, 226 ( будівля Укртелекому, 3 поверх )',
                 schedule: 'Пн, Ср, Пт: 15:30 - 16:45',
                 instructorId: instructorIds.Anatoliy
-            }
+        },
         {
                 ageCategory: 'Дорослі',
                 address: 'м. Мерефа, вул. Дніпровська, 226 ( будівля Укртелекому, 3 поверх )',
                 schedule: 'Пн, Ср, Пт: 17:00 - 18:15',
                 instructorId: instructorIds.Anatoliy
-            }
+        },
         ];
 
         contacts.getContacts = function() {
