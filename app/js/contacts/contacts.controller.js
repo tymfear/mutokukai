eb6ecb484f77cfbfc70b944a69906ab00377cc91
seliverstmax <154024685+seliverstmax@users.noreply.github.com>
2023-12-17 19:23:56 +0200

    Update contacts.controller.js
    Добавил Мерефу в росписание

--- app/js/contacts/contacts.controller.js
+++ app/js/contacts/contacts.controller.js
@@ -27,6 +27,24 @@
                 schedule: 'Пн, Пт: 07:00 - 08:30',
                 instructorId: instructorIds.Anatoliy
             }
+{
+                ageCategory: 'Діти',
+                address: ' м. Мерефа, вул. Дніпровська, 226 ( будівля Укртелекому 3 поверх )',
+                schedule: 'Пн, Ср, Пт: 14:15 - 15:15',
+                instructorId: instructorIds.Anatoliy
+            }
+        {
+                ageCategory: 'Підлітки',
+                address: 'м. Мерефа, вул. Дніпровська, 226 ( будівля Укртелекому 3 поверх )',
+                schedule: 'Пн, Ср, Пт: 15:30 - 16:45',
+                instructorId: instructorIds.Anatoliy
+            }
+        {
+                ageCategory: 'Дорослі',
+                address: 'м. Мерефа, вул. Дніпровська, 226 ( будівля Укртелекому 3 поверх )',
+                schedule: 'Пн, Ср, Пт: 17:00 - 18:15',
+                instructorId: instructorIds.Anatoliy
+            }
         ];
 
         contacts.getContacts = function() {
