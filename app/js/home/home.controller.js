0d77c55bc56a3ffc1845cd36109fecec2cfc9511
seliverstmax <154024685+seliverstmax@users.noreply.github.com>
2023-12-17 19:28:16 +0200

    Update home.controller.js
    Добавил 3 тренировки в Мерефе

--- app/js/home/home.controller.js
+++ app/js/home/home.controller.js
@@ -22,6 +22,33 @@
                     5: '07:00',
                 }
             },
+                                {
+                place: 'Merefa',
+                transport: bus,
+                schedule: {
+                    1: '14:15',
+                    3: '14:15',
+                    5: '14:15',  
+                }
+            },
+                                 {
+                place: 'Merefa',
+                transport: bus,
+                schedule: {
+                    1: '15:30',
+                    3: '15:30',
+                    5: '15:30',  
+                }
+            },
+                                 {
+                place: 'Merefa',
+                transport: bus,
+                schedule: {
+                    1: '17:00',
+                    3: '17:00',
+                    5: '17:00',  
+                }
+            },
         ];
     }
 })();
